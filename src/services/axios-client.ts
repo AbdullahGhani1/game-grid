7cea722a496fa8e3d783b035273361f7bb543ca9
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2025-11-14 16:20:29 +0500

    fixing workflow

--- src/services/axios-client.ts
+++ src/services/axios-client.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 axios.create({
-   baseURL: 'https://api.example.com',
+   baseURL: 'https://api.rawg.io/api/games',
    params: {
-      key: '',
+      key: 'c7b18323a47d40c394ea5b019646b1f5',
    },
    timeout: 1000,
    headers: { 'X-Custom-Header': 'foobar' },
